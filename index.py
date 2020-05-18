baaae9ce3567532b8d1092a96a6fe5051a7a8b44
yeonhodev <yeonho.dev@gmail.com>
2020-05-17 23:01:47 -0400

    initial commit

---
+++ index.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+print("content-type:text/html; charset=utf-8\n")
+print('''
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>yeonhodev.github.io</title>
+</head>
+<body>
+    <h1><a href="index.py">yeonhodev.github.io</a></h1>
+    <ol>
+        <li>Dev</li>
+            <ol>
+                <li>OS</li>
+                    <ol>
+                        <li>Mac</li>
+                        <li>Windows</li>
+                        <li><a href="index.py?id=linux">Linux</a></li>
+                    </ol>
+                <li>WEB</li>
+                    <ol>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>JavaScript</li>
+                        <li>SQL</li>
+                        <li><a href="index.py?id=python">Python</a></li>
+                    </ol>
+                <li>Mobile</li>
+                <li>LANGUAGES</li>
+                    <ol>
+                        <li>Python</li>
+                    </ol>
+                <li>Git</li>
+            </ol>
+    </ol>
+    <h2>Dev Blog</h2>
+    <p>I would like to be a programmer.</p>
+</body>
+</html>
+''')
