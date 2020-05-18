9c297186b614f947877139002072635ee7bd72da
yeonhodev <yeonho.dev@gmail.com>
2020-05-18 08:28:23 -0400

    Added boolean.py - Python Boolean syntax

---
+++ boolean.py
@@ -0,0 +1,9 @@
+#Comparison operator
+print(1==1) #Ture
+print(1<2) #True
+print(2<1) #False
+
+#Membership operator
+print('Hello' in 'Hello World') #True
+import os.path
+print(os.path.exists('boolean.py')) #True
