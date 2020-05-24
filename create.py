06ceeebf86205bd4500e3f9722f047dd0a72927e
yeonhodev <yeonho.dev@gmail.com>
2020-05-23 23:57:03 -0400

    Added create.py

---
+++ create.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+print("content-type:text/html; charset=utf-8\n")
+
+import cgi, os
+
+files = os.listdir('data')
+listStr = ''
+for item in files:
+    listStr = listStr + '<li><a href="index.py?id={name}">{name}</a></li>'.format(name=item)
+
+form = cgi.FieldStorage()
+
+if 'id' in form:
+    pageId = form["id"].value
+    description = open('data/'+pageId+'.html', 'r').read()
+else:
+    pageId = 'Dev Blog'
+    description = 'Hello, blog'
+
+print('''
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>yeonhodev.github.io</title>
+</head>
+<body>
+    <h1><a href="index.py">yeonhodev.github.io</a></h1>
+    <ol>
+        <li>Dev</li>
+            <ol>
+                <li>OS</li>
+                    <ol>
+                        <li>Mac</li>
+                        <li>Windows</li>
+                        <li><a href="index.py?id=linux">Linux</a></li>
+                    </ol>
+                <li>WEB</li>
+                    <ol>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>JavaScript</li>
+                        <li>SQL</li>
+                        <li><a href="index.py?id=python">Python</a></li>
+                    </ol>
+                <li>Mobile</li>
+                <li>LANGUAGES</li>
+                    <ol>
+                        <li>Python</li>
+                    </ol>
+                <li>Git</li>
+            </ol>
+    </ol>
+    <ol>
+        {listStr}
+    </ol>
+    <h2>{title}</h2>
+    <p>{desc}</p>
+</body>
+</html>
+'''.format(title=pageId, desc=description, listStr=listStr))
