dfa352c94f21cad100ae268038fd3be547feb761
yeonhodev <yeonho.dev@gmail.com>
2020-05-26 07:29:26 -0400

    Added an example of combination of for and if

--- syntax/dictionary.py
+++ syntax/dictionary.py
@@ -46,3 +46,15 @@ print("# 우리 동네 애완 동물들")
 for pet in pets:
     print("{} {}살".format(pet["name"], pet["age"]))
 
+#numbers 내부에 들어있는 숫자가 몇 번 등장하는지를 출력하는 코드
+numbers = [1,2,6,8,4,3,2,1,9,5,4,9,7,2,1,3,5,4,8,9,7,2,3]
+counter = {}
+
+for number in numbers:
+    if number in counter:
+        counter[number] += 1
+    else:
+        counter[number] = 1
+
+print()
+print(counter)
