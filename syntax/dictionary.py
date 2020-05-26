c0696f8d44c485835393263a4e0476898d8b3c73
yeonhodev <yeonho.dev@gmail.com>
2020-05-26 06:34:07 -0400

    Added new syntax. - Adding/deleting an element from dictionary

--- syntax/dictionary.py
+++ syntax/dictionary.py
@@ -1,8 +1,22 @@
+#Create a new dictionary
 dictionary1 = {
     "keyA": "valueA",
     273: [1, 2, 3, 4],
      True: False
 }
 
+#Use with for loop
+for key in dictionary1: 
+    print("{} : {}".format(key, dictionary1[key]))
+
+#Add a new element
+dictionary1["keyB"] = "valueB"
+print()
+for key in dictionary1: 
+    print("{} : {}".format(key, dictionary1[key]))
+
+#Delete a new element
+del dictionary1["keyB"]
+print()
 for key in dictionary1: 
     print("{} : {}".format(key, dictionary1[key]))
