cee7350fbea351bbe0af855377080ff3f182b6b0
yeonhodev <yeonho.dev@gmail.com>
2020-05-27 23:36:14 -0400

    flattening - Use for and if - Convert complicated dictionary to simpler format

--- syntax/dictionary.py
+++ syntax/dictionary.py
@@ -58,3 +58,25 @@ for number in numbers:
 
 print()
 print(counter)
+
+#Falttening
+character = {
+    "name": "기사",
+    "level": 12,
+    "items": {
+        "sword": "불꽃의 검", 
+        "armor": "풀플레이트"
+    },
+     "skill": ["베기", "세게 베기", "아주 세게 베기"]
+}
+
+print()
+for key in character:
+    if type(character[key]) is dict:
+        for k in character[key]:
+            print("{} : {}".format(k, character[key][k]))
+    elif type(character[key]) is list:
+        for item in character[key]:
+            print("{} : {}".format(key, item))
+    else:
+        print("{} : {}".format(key, character[key]))
