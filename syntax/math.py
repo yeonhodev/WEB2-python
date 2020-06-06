ef073a90490f6f25a30525a88ca1813b61ec4874
yeonhodev <yeonho.dev@gmail.com>
2020-06-05 23:59:38 -0400

    Added module.py and math.py

---
+++ syntax/math.py
@@ -0,0 +1,10 @@
+def average(a,b,c):
+    s=a+b+c
+    r=s/3
+    return r
+
+def plus(a,b):
+    s=a+b
+    return s
+
+pi = 3.14
---
+++ syntax/module.py
@@ -0,0 +1,13 @@
+'''
+import math
+
+print(math.average(1,2,3))
+print(math.plus(3,4))
+print(math.pi)
+'''
+
+from math import average, plus, pi
+
+print(average(1,2,3))
+print(plus(3,4))
+print(pi)
