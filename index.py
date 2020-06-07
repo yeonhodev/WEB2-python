40b42786c0eff2d06590c8d5329bfa5128cb5091
yeonhodev <yeonho.dev@gmail.com>
2020-06-07 01:12:03 -0400

    imported html-sanitizer

--- index.py
+++ index.py
@@ -2,14 +2,17 @@
 print("content-type:text/html; charset=utf-8\n")
 
 import cgi, os, view
+from html_sanitizer import Sanitizer
+sanitizer = Sanitizer()
 
 form = cgi.FieldStorage()
 
 if 'id' in form:
     pageId = form["id"].value
     description = open('data/'+pageId+'.html', 'r').read()
-    description = description.replace('<', '&lt;')
-    description = description.replace('>', '&gt;')
+    # description = description.replace('<', '&lt;')
+    # description = description.replace('>', '&gt;')
+    # description = sanitizer.sanitize(description)
     update_link = '<a href="update.py?id={}">update</a>'.format(pageId)
     delete_action = '''
         <form action="process_delete.py" method="post">
